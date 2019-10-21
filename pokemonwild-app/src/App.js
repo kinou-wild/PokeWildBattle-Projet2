a4f8329dc4bb3b3548644917f30a759787e81af3
kinou-wild <kevin.thomann13@gmail.com>
2019-10-21 12:53:56 +0200

    add Pokedex.js initialisation

--- pokemonwild-app/src/App.js
+++ pokemonwild-app/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Particle from './components/Particle'
-class App extends React.Component{
-  
-    render(){
-        return (
-          <Particle />
-        )
-    }}
+import Particle from './components/Particle';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <>
+        <Particle />
+      </>
+    )
+  }
+}
 export default App;
---
+++ pokemonwild-app/src/components/pokedex/Pokedex.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Pokedex.css';
+
+class Pokedex extends React.Component {
+
+    render() {
+        return (
+            <>
+            
+            </>
+        )
+    
+    }
+}
+
+export default Pokedex;
