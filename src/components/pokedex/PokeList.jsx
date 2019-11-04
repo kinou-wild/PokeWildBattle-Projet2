52943a0b7ff9c387e8b6ecf19a7f99dedad7c353
Jean Nicolas de Lamballerie <jean.nicolasdelamballerie@gmail.com>
2019-11-04 16:28:13 +0100

    Inclue la searchbar et la liste responsive à l'écriture

--- src/components/pokedex/PokeList.jsx
+++ src/components/pokedex/PokeList.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import "./PokeList.css";
 import Versus from './Versus'
 import PokeModal from './PokeModal'
-
+import MyInput from './Searchbar'
 
 const random = Math.floor(Math.random() * 151)
 
@@ -18,11 +18,13 @@ class PokeList extends React.Component {
             keyPokemon: [],
 
             keyComputer: [],
-            show: false
+            show: false,
+            research : "",
         }
+        this.setResearch=this.setResearch.bind(this);
 
     }
-
+    setResearch=(e)=>{this.setState({research:e})};
 
     showModal = e => {
         this.setState({
@@ -42,10 +44,18 @@ class PokeList extends React.Component {
         return (
              <div onLoad={() => this.setState({keyComputer: random})}>
             <div className="pokeListDiv">
+                <p>{this.state.research}</p>
+            <div className='Search'>
+                {/* <Searchbar handleResearch={this.setResearch} suggestions={this.state.results.map(x => x.name)}
+/> */}
+<MyInput suggestions={this.state.pokemons.results.map(x=>x.name)} handler = {this.setResearch}></MyInput>
+</div>
              <div className="center"> 
             <PokeModal keyModal={this.state.keyPokemon} onClose={this.showModal} show={this.state.show} />
 
-                    {this.state.pokemons.results.map((x, y) =>
+                    {this.state.pokemons.results.map((x, y) => {
+                    {if (this.state.research === "") {
+                        return(
                         <div className="pokeCard" key={y} onClick={() => this.setState({ 
                             namePokemon: x.name, 
                             keyPokemon: y
@@ -54,7 +64,21 @@ class PokeList extends React.Component {
                             <img className="pokeImg" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${y + 1}.png`} alt="front sprite on card" />
                             <hr className="cardHr"></hr>
                             <p className="pokeName">{x.name}</p>
-                        </div>
+                        </div>)}
+                    else if (x.name.includes(this.state.research)) {
+                        return(
+                            <div className="pokeCard" key={y} onClick={() => this.setState({ 
+                            namePokemon: x.name, 
+                            keyPokemon: y
+                            },
+                             e => { this.showModal(e) })}>
+                            <img className="pokeImg" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${y + 1}.png`} alt="front sprite on card" />
+                            <hr className="cardHr"></hr>
+                            <p className="pokeName">{x.name}</p>
+                        </div>                            
+                        )}
+                    }
+                        }
 
                     )}
                 </div>
