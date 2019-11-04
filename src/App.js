279c6d72816f30f89f187dc96d4348850fea258c
kinou-wild <kevin.thomann13@gmail.com>
2019-11-04 16:06:09 +0100

    merge accueil into pokedex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Router from './components/Router'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class App extends React.Component {
 
   render() {
--- src/components/pokedex/PokeList.jsx
+++ src/components/pokedex/PokeList.jsx
@@ -11,66 +11,64 @@ class PokeList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            pokemons:{
-                results:[]
+            pokemons: {
+                results: []
             },
             namePokemon: ['Bulbazaur'],
             keyPokemon: [],
-
             keyComputer: [],
-            show: false
+            modal : false
         }
+        
+        this.changeStateModal = this.changeStateModal.bind(this)
+        
+    }
+
+    changeStateModal () {
+        this.setState({modal : !this.state.modal})
 
     }
 
-
-    showModal = e => {
-        this.setState({
-            show: !this.state.show
-        })
-    }
-
+    
     componentDidMount() {
         axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
             .then(res => {
-                this.setState({pokemons: res.data})
+                this.setState({ pokemons: res.data })
             })
     }
-    
+
     render() {
 
         return (
-             <div onLoad={() => this.setState({keyComputer: random})}>
-            <div className="pokeListDiv">
-             <div className="center"> 
-            <PokeModal keyModal={this.state.keyPokemon} onClose={this.showModal} show={this.state.show} />
+            <div onLoad={() => this.setState({ keyComputer: random })}>
+                <div className="pokeListDiv">
+                    <div className="center">
+                        <PokeModal keyModal={this.state.keyPokemon} changeStateModal={this.changeStateModal} stateModal={this.state.modal} namePokemon={this.state.namePokemon}/>
+                        {this.state.pokemons.results.map((x, y) =>
+                            <div className="pokeCard" key={y} onClick={() => this.setState({
+                                namePokemon: x.name,
+                                keyPokemon: y,
+                                modal:!this.state.modal
+                            })}>
+                                <img className="pokeImg" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${y + 1}.png`} alt="front sprite on card" />
+                                <hr className="cardHr"></hr>
+                                <p className="pokeName">{x.name}</p>
+                            </div>
 
-                    {this.state.pokemons.results.map((x, y) =>
-                        <div className="pokeCard" key={y} onClick={() => this.setState({ 
-                            namePokemon: x.name, 
-                            keyPokemon: y
-                            },
-                             e => { this.showModal(e) })}>
-                            <img className="pokeImg" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${y + 1}.png`} alt="front sprite on card" />
-                            <hr className="cardHr"></hr>
-                            <p className="pokeName">{x.name}</p>
-                        </div>
-
-                    )}
+                        )}
+                    </div>
                 </div>
-            </div>
-            <Versus 
-            nameVs={this.state.namePokemon} 
-            photoVs={this.state.keyPokemon} 
+                <Versus
+                    nameVs={this.state.namePokemon}
+                    photoVs={this.state.keyPokemon}
+                    keyC={random} />
 
-            keyC={random}/>
-            
-            
-        </div>
+
+            </div>
         )
-        
+
     }
-   
+
 }
 
 
