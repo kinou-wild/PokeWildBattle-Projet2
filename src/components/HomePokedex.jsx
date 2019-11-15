cc38e9fc30fdcd86540b5017501e4538bb0ba6d0
kinou-wild <kevin.thomann13@gmail.com>
2019-11-15 10:07:35 +0100

    ajout de 3 musiques finales

--- src/components/HomePokedex.jsx
+++ src/components/HomePokedex.jsx
@@ -26,7 +26,7 @@ class HomePokedex extends React.Component {
                 <Particle />
                 <Pokedex />
                 <Footer />
-                <iframe className="music-pokedex" width="510" height="315" src="https://www.youtube.com/embed/5NmjzE-zmlk?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>        
+                <iframe className="music-pokedex" width="510" height="315" src="https://www.youtube.com/embed/5NmjzE-zmlk?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>     
             </div>
         );
     }
