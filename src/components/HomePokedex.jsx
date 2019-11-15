fe7980b8201dcf5c0b5354a8f936acccf4010a50
kinou-wild <kevin.thomann13@gmail.com>
2019-11-15 11:29:37 +0100

    ajout de react player à la place de balise iframe pour les musiques

--- src/components/HomePokedex.jsx
+++ src/components/HomePokedex.jsx
@@ -4,6 +4,7 @@ import Pokedex from './pokedex/Pokedex'
 import Loading from './Loading'
 import Footer from './Footer/Footer'
 import './HomePokedex.css'
+import ReactPlayer from 'react-player'
 
 
 class HomePokedex extends React.Component {
@@ -26,7 +27,17 @@ class HomePokedex extends React.Component {
                 <Particle />
                 <Pokedex />
                 <Footer />
-                <iframe className="music-pokedex" width="510" height="315" src="https://www.youtube.com/embed/5NmjzE-zmlk?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>     
+                <ReactPlayer className="music-pokedex" url="https://www.youtube.com/embed/5NmjzE-zmlk" playing
+            controls
+                config={{
+                    file: {
+                        attributes: {
+                            autoPlay: true,
+                            
+                        }
+                    }
+                }}
+            />
             </div>
         );
     }
--- src/components/accueil/OnclickAccueil.jsx
+++ src/components/accueil/OnclickAccueil.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './onclickaccueil.css'
 import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
 
 
@@ -8,7 +9,17 @@ const OnclickAccueil = () => {
     return (
         <>
             <Link className='accueil' to={'/pokedex'}></Link>
-            <iframe className="music-home" width="560" height="315" src="https://www.youtube.com/embed/iOCMgLKIPmQ?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <ReactPlayer className="music-home" url="https://www.youtube.com/embed/iOCMgLKIPmQ" playing
+            controls
+                config={{
+                    file: {
+                        attributes: {
+                            autoPlay: true,
+                            
+                        }
+                    }
+                }}
+            />
         </>
     )
 }
